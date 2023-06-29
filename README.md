7819158aee48b21e977129276b1c63ceb4b3d2cb
Vysakh P <vysakhp17@gmail.com>
2023-06-29 19:13:35 +0530

    body-parser included and get data from body

--- README.md
+++ README.md
@@ -1,5 +1,14 @@
 # RESTful-API
 REST API learning with mongodb
+
 Automatically reloading using nodemon
+
 default logging using morgan
+
 Morgan is a logging package for node.js. It can generate log files for all the traffic that comes to your server. It has some cool tricks to help us in both development and production.
+
+body-parser
+
+Node.js body parsing middleware.
+
+Parse incoming request bodies in a middleware before your handlers, available under the `req.body` property.
--- app.js
+++ app.js
@@ -1,11 +1,15 @@
 const express = require('express');
 const app = express();
 const morgan = require('morgan');
+const bodyParser = require('body-parser');
 
 const productRoutes = require('./api/routes/products')
 const orderRoutes = require('./api/routes/order')
 
 app.use(morgan('dev'));
+app.use(bodyParser.urlencoded({ extended: false })); // true allows you to parse extended bodies rich data
+app.use(bodyParser.json()); 
+
 
 app.use('/products', productRoutes);
 app.use('/orders', orderRoutes);
