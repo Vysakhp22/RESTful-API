f82e0f94441da57515b50be9555b6dbf03e7df26
Vysakh P <vysakhp17@gmail.com>
2023-07-02 08:55:33 +0530

    PATCH method updated

--- api/routes/products.js
+++ api/routes/products.js
@@ -66,9 +66,22 @@ router.get('/:productId', (req, res, next) => {
 });
 
 router.patch('/:productId', (req, res, next) => {
-    res.status(200).json({
-        message: 'updated product..!'
-    });
+    const id = req.params.productId;
+    const updateOps = {};
+    for (const ops of req.body) {
+        updateOps[ops.propName] = ops.value;
+    }
+    Product.updateOne({ _id: id }, { $set: updateOps })
+        .exec()
+        .then((result) => {
+            res.status(200).json(result);
+        }) 
+        .catch(err => {
+            console.error(err);
+            res.status(500).json({
+                error: err
+            });
+        });
 });
 
 router.delete('/:productId', (req, res, next) => {
