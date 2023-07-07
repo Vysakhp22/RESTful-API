e9ade460dbd6c39de9ae2cbe490b009d69568450
Vysakh P <vysakhp17@gmail.com>
2023-07-07 08:51:35 +0530

    PR changes added, productId in orders set as required

--- api/models/order.js
+++ api/models/order.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const orderSchema = mongoose.Schema({
     _id: mongoose.Schema.Types.ObjectId,
-    product: { type: mongoose.Schema.Types.ObjectId, ref: 'Product' }, //used to keep a relation (ref is string & value is name of the model you want to connect)
+    product: { type: mongoose.Schema.Types.ObjectId, ref: 'Product', required: true }, //used to keep a relation (ref is string & value is name of the model you want to connect)
     quantity: { type: Number, default: 1 } // it can be set as required or can set a default value both are same
 });
 
