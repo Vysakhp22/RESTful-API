fe0556e487f3aeda0195390f50021cde28ba015f
Vysakh P <vysakhp17@gmail.com>
2023-07-11 22:06:12 +0530

    Validate user

--- api/routes/user.js
+++ api/routes/user.js
@@ -6,31 +6,45 @@ const bcrypt = require('bcrypt');
 const User = require('../models/user');
 
 router.post('/signup', (req, res, next) => {
-    password: bcrypt.hash(req.body.password, 10, (err, hash) => {
-        if (err) {
-            return res.status(500).json({
+    User.find({ email: req.body.email })
+        .exec()
+        .then(v => {
+            if (v) {
+                return res.status(409).json({
+                    message: "User Already Exist"
+                });
+            } else {
+                password: bcrypt.hash(req.body.password, 10, (err, hash) => {
+                    if (err) {
+                        return res.status(500).json({
+                            error: err
+                        });
+                    } else {
+                        const user = new User({
+                            _id: new mongoose.Types.ObjectId(),
+                            email: req.body.email,
+                            password: hash
+                        });
+                        user.save()
+                            .then(result => {
+                                console.log(result);
+                                res.status(201).json({
+                                    message: "User created"
+                                });
+                            })
+                            .catch(err => {
+                                res.status(500).json({
+                                    error: err
+                                });
+                            });
+                    }
+                })
+            }
+        }).catch(err => {
+            res.status(500).json({
                 error: err
             });
-        } else {
-            const user = new User({
-                _id: new mongoose.Types.ObjectId(),
-                email: req.body.email,
-                password: hash
-            });
-            user.save()
-                .then(result => {
-                    console.log(result);
-                    res.status(201).json({
-                        message: "User created"
-                    });
-                })
-                .catch(err => {
-                    res.status(500).json({
-                        error: err
-                    });
-                });
-        }
-    })
+        });
 });
 
 module.exports = router
